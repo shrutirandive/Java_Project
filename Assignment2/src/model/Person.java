2786dba973be9af9f1d93fde4506bfe358fdf4b1
shrutirandive <shrutirandive4@gmail.com>
2022-10-19 18:01:23 -0400

    add person

--- Assignment2/src/model/Person.java
+++ Assignment2/src/model/Person.java
@@ -9,5 +9,77 @@ package model;
  * @author user
  */
 public class Person {
+    String userName;
+    String password;
+    String user_type;
+    String name;
+    int age;
+    String gender;
+    String address;
+    String city;
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(String user_type) {
+        this.user_type = user_type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
